ed565d1e65158147289a63d7b61b7da8c53eef63
Agnes Ribeiro <mu21@hotmail.fr>
2018-10-15 11:40:49 +0200

    filter exo2

--- filter/ex2.js
+++ filter/ex2.js
@@ -16,6 +16,10 @@ Exemple d'entrée:
  */
 
 function getMultiplesOf(numbers, n) {
+  const multipleNumbers = numbers.filter(function(num) {
+    return num % n === 0;
+});
+return multipleNumbers;
 }
 
 // Ne pas modifier l'export
