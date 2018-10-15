a5b058251383354ea40c6bf2336d0cdaacd894ff
Agnes Ribeiro <mu21@hotmail.fr>
2018-10-15 17:56:41 +0200

    map+filter exo1

--- filter-map/ex1.js
+++ filter-map/ex1.js
@@ -5,7 +5,8 @@ de personnages de Star Wars (objets), chacun ayant deux propriétés :
 - name: nom du personnage
 - side: le côté de la Force où il se situe ('light' ou 'dark')
 
-En utilisant filter puis map, la fonction doit renvoyer les noms des Jedis, c'est-à-dire les personnages du côté lumineux (pour ceux qui auraient passé les 40 dernières années dans une caverne en Lozère !)
+En utilisant filter puis map, la fonction doit renvoyer les noms des Jedis, c'est-à-dire les personnages du côté lumineux
+ (pour ceux qui auraient passé les 40 dernières années dans une caverne en Lozère !)
 
 Tableau en entrée:
 [
@@ -23,6 +24,14 @@ Sortie attendue:
 */
 
 function getJedisNames(characters) {
+  const goodCharacter = characters.filter(function(character) {
+    return character.side === 'light';
+  })
+  .map(function(character) {
+    return character.name;
+  });
+  return goodCharacter;
 }
+
 
 module.exports = getJedisNames;
